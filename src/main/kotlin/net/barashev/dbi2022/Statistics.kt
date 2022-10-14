d994a7c91314b0cee0b1caf3ffbbffe1f84baad5
Dmitry Barashev <dmitry@barashev.net>
2022-10-15 02:26:52 +0300

    - doc comments - bucketCount

--- src/main/kotlin/net/barashev/dbi2022/Statistics.kt
+++ src/main/kotlin/net/barashev/dbi2022/Statistics.kt
@@ -2,7 +2,17 @@ package net.barashev.dbi2022
 
 import java.util.function.Function
 
-data class HistogramBucket<T>(val rangeStart: T, val rangeEnd: T, val valueCount: Int)
+/**
+ * A single bucket of an attribute value histogram.
+ * A bucket indicates how many values are in a semi-open range [rangeStart, rangeEnd)
+ */
+data class HistogramBucket<T>(val rangeBegin: T, val rangeEnd: T, val valueCount: Int)
+
+/**
+ * A single attribute statistics which includes:
+ * - attribute cardinality (total number of distinct attribute values)
+ * - an equi-height histogram of the attribute values distribution
+ */
 data class AttributeStatistics<T: Comparable<T>>(
     val attributeName: String,
     val cardinality: Int,
@@ -10,13 +20,25 @@ data class AttributeStatistics<T: Comparable<T>>(
 )
 
 interface StatisticsManager {
+    /**
+     * Creates statistics for the given attribute of a table, using specified bucketCount number of histogram buckets.
+     * Statistic data shall be persistent, and shall be returned from getStatistics calls without re-calculating
+     */
     fun <T: Comparable<T>> buildStatistics(
-        tableName: String, attributeName: String, attributeType: AttributeType<T>, attributeValue: Function<ByteArray, T>): AttributeStatistics<T>
+        tableName: String, attributeName: String, attributeType: AttributeType<T>, bucketCount: Int, attributeValue: Function<ByteArray, T>): AttributeStatistics<T>
 
+    /**
+     * @return Returns statistics previously build for the given attribute of a table, or null if no statistics records
+     * were found for that attribute.
+     */
     fun <T: Comparable<T>> getStatistics(tableName: String, attributeName: String, attributeType: AttributeType<T>): AttributeStatistics<T>?
 }
 
 object Statistics {
+    /**
+     * Factory method for creating StatisticsManager instances.
+     * Please set your own factory instance from your own code.
+     */
     var managerFactory: (AccessMethodManager, PageCache) -> StatisticsManager = { _, _ -> FakeStatisticsManager() }
 }
 
@@ -26,6 +48,7 @@ private class FakeStatisticsManager: StatisticsManager {
         tableName: String,
         attributeName: String,
         attributeType: AttributeType<T>,
+        bucketCount: Int,
         attributeValue: Function<ByteArray, T>
     ): AttributeStatistics<T> = AttributeStatistics(attributeName, 0, emptyList())
 
