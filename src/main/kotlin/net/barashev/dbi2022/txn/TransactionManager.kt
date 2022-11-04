4f9a7b12d8bcfa1da0c391ecc68f13199fed8479
Dmitry Barashev <dmitry@barashev.net>
2022-11-04 16:01:29 +0300

    LogManager, WAL, Recovery

--- src/main/kotlin/net/barashev/dbi2022/txn/TransactionManager.kt
+++ src/main/kotlin/net/barashev/dbi2022/txn/TransactionManager.kt
@@ -9,41 +9,49 @@ import net.barashev.dbi2022.*
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicInteger
 
-class TransactionManager(private val scheduler: Scheduler, val cache: PageCache): Scheduler by scheduler {
+typealias TxnWriteTracker = (PageId, Boolean) -> Unit
+
+class TransactionManager(private val scheduler: Scheduler, val logManager: LogManager): Scheduler by scheduler {
     private val txnWorkerScope = CoroutineScope(Executors.newCachedThreadPool().asCoroutineDispatcher())
     private val lastTxnId = AtomicInteger(0)
     private val txnCompletionScope = CoroutineScope(Executors.newSingleThreadExecutor().asCoroutineDispatcher())
     private val txnCompletionChannel = Channel<TransactionDescriptor>()
-
     override fun commit(txn: TransactionDescriptor): List<TransactionDescriptor> =
         scheduler.commit(txn).also {
+            logManager.commit(txn)
             txnCompletionScope.launch { txnCompletionChannel.send(txn) }
         }
 
     override fun abort(txn: TransactionDescriptor): List<TransactionDescriptor> =
         scheduler.abort(txn).also {
+            logManager.abort(txn)
             txnCompletionScope.launch { txnCompletionChannel.send(txn) }
         }
 
     fun txn(txnCode: suspend (PageCache, TransactionDescriptor) -> Unit) {
         val txnId = lastTxnId.incrementAndGet()
-        val txnPageCache = TxnPageCacheImpl(cache, txnId, this, txnCompletionChannel)
         txnWorkerScope.launch {
-            txnCode(txnPageCache, txnId)
+            val txnPageCache = TxnPageCacheImpl(logManager.pageCache, txnId, this@TransactionManager, txnCompletionChannel, logManager.start(txnId))
+            try {
+                txnCode(txnPageCache, txnId)
+            } catch (ex: Exception) {
+                abort(txnId)
+            }
         }
     }
-
 }
 
 internal class TxnPageCacheImpl(
     private val proxiedCache: PageCache,
     private val txn: TransactionDescriptor,
     private val scheduler: Scheduler,
-    private val txnCompletionChannel: Channel<TransactionDescriptor>
+    private val txnCompletionChannel: Channel<TransactionDescriptor>,
+    private val writeTracker: TxnWriteTracker
 ) : PageCache by proxiedCache {
     override fun get(pageId: PageId): CachedPage = tryRead(pageId) { proxiedCache.get(pageId) }
 
     override fun getAndPin(pageId: PageId): CachedPage = tryRead(pageId) { proxiedCache.getAndPin(pageId) }
+
 
     private fun tryRead(pageId: PageId, code: () -> CachedPage): CachedPage {
         while (true) {
@@ -75,7 +83,8 @@ internal class TxnPageCacheImpl(
         while (true) {
             when (val write = scheduler.write(txn, pageId)) {
                 is WriteOk -> {
-                    return code()
+                    writeTracker(pageId, false)
+                    return code().also { writeTracker(pageId, true) }
                 }
 
                 is WriteAbort -> {
@@ -98,7 +107,7 @@ internal class TxnPageCacheImpl(
     }
 
     override fun createSubCache(size: Int): PageCache =
-        TxnPageCacheImpl(proxiedCache.createSubCache(size), txn, scheduler, txnCompletionChannel)
+        TxnPageCacheImpl(proxiedCache.createSubCache(size), txn, scheduler, txnCompletionChannel, writeTracker)
 }
 
 internal class TxnPage(private val proxiedPage: CachedPage, private val txnCache: TxnPageCacheImpl): CachedPage by proxiedPage {
